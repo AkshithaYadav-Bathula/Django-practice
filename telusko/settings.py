6ece7874c9ca10b019ebafee1ae01ae268f318a2
unknown <akshithayadavbathula27@gmail.com>
2026-03-20 13:12:18 +0530

    ORM + model + postgres

--- telusko/settings.py
+++ telusko/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
     }
 }
 
@@ -122,3 +126,6 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- travello/views.py
+++ travello/views.py
@@ -5,10 +5,30 @@ def index(request):
     # return HttpResponse("Hello World")
     
     # return render(request,'home.html', {'name':'Akshitha'})
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc='The city that never sleeps'
-    dest1.price = 999
+    # dest1 = Destination()
+    # dest1.name = 'Mumbai'
+    # dest1.desc='The city that never sleeps'
+    # dest1.img = '/static/images/destination_1.jpg'
+    # dest1.price = 999
+    # dest1.offer = False
+
+    # dest2=Destination()
+    # dest2.name = "Paris"
+    # dest2.img = '/static/images/destination_2.jpg'
+    # dest2.desc = "The city of Love"
+    # dest2.price = 552
+    # dest2.offer = True
+
+    # dest3 = Destination()
+    # dest3.name = "Switzerland"
+    # dest3.img = '/static/images/destination_3.jpg'
+    # dest3.desc = "The city of cold and snow"
+    # dest3.price = 505
+    # dest3.offer = False
+
+    # dests = [dest1,dest2,dest3]
     # return render(request,'index.html', {'price':700}) 
-    return render(request, 'index.html', {'dest1':dest1})
+    # return render(request, 'index.html', {'dest1':dest1, 'dest2':dest2, 'dest3':dest3})
+    dests = Destination.objects.all() #from database it is coming
+    return render(request,'index.html', {'dests':dests})
 #send req in rsponse
